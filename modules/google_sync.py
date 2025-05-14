c3ed9402e9a092a46fcc58fd8c562a865232bfdf
Noman-QistBazar <mnomanqb@gmail.com>
2025-05-14 07:55:24 -0700

    Update google_sync.py

--- modules/google_sync.py
+++ modules/google_sync.py
@@ -1,17 +1,20 @@
-import gspread
-import pandas as pd
-from gspread_dataframe import set_with_dataframe
 from google.oauth2 import service_account
 import streamlit as st
+import gspread
+from gspread_dataframe import set_with_dataframe
+import pandas as pd
 from modules.utils import DATA_FILE
 
 def save_to_google_sheets():
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
-        scopes=["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
     )
     client = gspread.authorize(credentials)
-    spreadsheet = client.open("Rider Slip Data")  # make sure this name matches your sheet
+    spreadsheet = client.open("Rider Slip Data")
 
     all_data = pd.read_excel(DATA_FILE, sheet_name=None)
     for sheet_name, df in all_data.items():
